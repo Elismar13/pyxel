494966b69a99bcef8107c10bce5e9be050b4dd06
Elismar13 <elismarsilva.5@outlook.com>
2021-05-11 12:01:27 -0300

    :sparkles: (chore) to rgb converter and pixel count by color method

--- src/pyxel/Pyxel.py
+++ src/pyxel/Pyxel.py
@@ -9,6 +9,22 @@ class Pyxel():
   def openImage(self, filePath: str):
     self.image = Image.open(filePath)
     self.imageDimensions = self.image.size
-  
-  
 
+  def pixelColorCount(self):
+    rgb_image = self.toRGB()
+    width, height = self.imageDimensions
+    pixel_color_count = dict()
+
+    for x in range(width):
+      for y in range(height):
+        pixel = rgb_image.getpixel((x, y))
+
+        if pixel in pixel_color_count:
+          pixel_color_count[pixel] += 1
+        else:
+          pixel_color_count[pixel] = 1
+    
+    return pixel_color_count
+
+  def toRGB(self):
+    return self.image.convert('RGB')
